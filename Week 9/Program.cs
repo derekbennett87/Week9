b4aadb3c0de0d8cc6e2c24d3ad8ad0a5b41dd248
derekbennett87 <98566408+derekbennett87@users.noreply.github.com>
2022-04-01 18:51:20 -0500

    Wee9 working

---
+++ Week 9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace Week_9
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a file path: ");
+
+            string line;
+
+            string input = Console.ReadLine();
+
+            var fpath = new Regex(@"^[A-Z]\S.[A-Za-z0-9]+\S.+\.[A-Za-z0-9]{3}$");
+
+
+            if (fpath.IsMatch(input)) ;
+            {
+
+                File.ReadLines(input);
+
+                string[] words = File.ReadAllLines(input);
+
+                int count = 0;
+
+                for (int i = 0; i < words.Length; i++)
+
+                    count += words[i].Split(' ').Length;
+
+                Console.WriteLine(" File path is valid \n Opening file.....\n There are " + count +
+                " words in this file friends");
+            }
+        }
+    }
+}
+ 
+
